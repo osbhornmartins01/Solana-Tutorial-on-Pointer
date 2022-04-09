f420d2f677ff857bae37840ceb6fa51544568f1e
Osbhorn Martins <101728374+osbhornmartins01@users.noreply.github.com>
2022-04-09 17:18:17 +0100

    Add files via upload

---
+++ lib/addresses.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('7NdrQJozhYWWy1CXCfnbAYKY77raY7J5SMKfZYa7Cd5S')
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('H4y61sjbCMHyaX5ZAx9j3V5x443yRcuSrzGETe5JJn8C')
